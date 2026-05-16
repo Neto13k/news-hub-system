f1fc48ca71a84b922052b4897cf0cb989f64a68b
José Hermes <jose.hermes.dev@gmail.com>
2026-05-16 20:49:17 -0300

    chore: adiciona comentários explicativos nas rotas

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -1,10 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-   index("routes/home.tsx"),
-   route("login", "routes/login.tsx"),
-   route("register", "routes/register.tsx"),
-   route("posts", "routes/posts.tsx"),
-   route("post/:id", "routes/post.tsx")
+   index("routes/home.tsx"), // Página inicial
+   route("login", "routes/login.tsx"), // Página de login
+   route("register", "routes/register.tsx"), // Página de registro
+   route("posts", "routes/posts.tsx"), // Listagem de todos os posts
+   route("post/:id", "routes/post.tsx") // Detalhe de um post específico
 
 ] satisfies RouteConfig;
