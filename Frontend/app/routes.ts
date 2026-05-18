d671debbae617e669a9a6e0c5dc7fb0fb6d0adbf
José Hermes <jose.hermes.dev@gmail.com>
2026-05-18 20:25:48 -0300

    fix: corrigir caminhos das rotas no arquivo routes.ts

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -1,10 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-   index("routes/home.tsx"), // Página inicial
-   route("login", "routes/login.tsx"), // Página de login
-   route("register", "routes/register.tsx"), // Página de registro
-   route("posts", "routes/posts.tsx"), // Listagem de todos os posts
-   route("post/:id", "routes/post.tsx") // Detalhe de um post específico
+   index("routes/home/home.tsx"), // Página inicial
+   route("login", "routes/login/login.tsx"), // Página de login
+   route("register", "routes/register/register.tsx"), // Página de registro
+   route("posts", "routes/post/posts.tsx"), // Listagem de todos os posts
+   route("post/:id", "routes/post/post.tsx") // Detalhe de um post específico
 
 ] satisfies RouteConfig;
