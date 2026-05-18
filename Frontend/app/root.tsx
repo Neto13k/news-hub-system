fa8e1e18961eaa763ff367019ee059be7ba0e0b5
José Hermes <jose.hermes.dev@gmail.com>
2026-05-18 19:55:28 -0300

    chore: remove duplicação da importação do arquivo de estilos no root.tsx

--- Frontend/app/root.tsx
+++ Frontend/app/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import "./styles/main.scss";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
