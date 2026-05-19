a8e98e17b6cacd1ab4ecde9ecd0a5f64538e0c31
José Hermes <jose.hermes.dev@gmail.com>
2026-05-19 20:40:50 -0300

    refactor: adicionar comentário explicativo na página de login

--- Frontend/app/routes/login/login.tsx
+++ Frontend/app/routes/login/login.tsx
@@ -1,5 +1,6 @@
 import Login from "../../components/login";
 
+// Página de rota de login que renderiza o componente de formulário de login
 export default function LoginPage() {
     return <Login />
 }
