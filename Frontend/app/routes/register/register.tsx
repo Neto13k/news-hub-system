06c014405e41fdb6e4cff9a5352efed002ab8dc6
José Hermes <jose.hermes.dev@gmail.com>
2026-05-19 20:40:56 -0300

    refactor: adicionar comentário explicativo na página de registro

--- Frontend/app/routes/register/register.tsx
+++ Frontend/app/routes/register/register.tsx
@@ -1,5 +1,6 @@
 import Register from "../../components/register";
 
+// Página de rota de registro que renderiza o componente de formulário de cadastro
 export default function RegisterPage() {
     return <Register />
 }
