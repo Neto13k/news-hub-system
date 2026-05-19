7bedf0221ac5fb8df75538a4be70c1281ace18a6
José Hermes <jose.hermes.dev@gmail.com>
2026-05-18 21:03:05 -0300

    feat: implementar componente de login com formulário utilizando react-hook-form

--- Frontend/app/components/login.tsx
+++ Frontend/app/components/login.tsx
@@ -1,7 +1,29 @@
-function teste() {
-    console.log("Teste");
-    return <h1>Teste</h1>
+import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router";
+
+
+export default function Login() {
+    const navigate = useNavigate();
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        navigate("/login");
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" {...register("email")} />
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" {...register("password")} />
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                Não tem login? <Link to="/register">Cadastre-se aqui.</Link>
+            </p>
+        </>
+    );
 }
-
-export default teste;
-
