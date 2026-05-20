6d434cd116a1d14ac8bf67d19b8f82d8df928abd
José Hermes <jose.hermes.dev@gmail.com>
2026-05-20 20:46:19 -0300

    fix: substitui tag <a> por Link do React Router na listagem de posts

--- Frontend/app/components/posts.tsx
+++ Frontend/app/components/posts.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router";
 import { getPosts } from "../services/api";
 
 interface Post {
@@ -26,7 +27,7 @@ export default function RenderPosts() {
       <h1>Posts</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.title} <Link to={`/post/${post.id}`}>View Details</Link></li>
         ))}
       </ul>
     </div>
