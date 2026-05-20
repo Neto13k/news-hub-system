521590fc340df8a78a7ea46dd69a2e36a59a0675
José Hermes <jose.hermes.dev@gmail.com>
2026-05-20 20:46:10 -0300

    feat: adiciona rota de criação de post

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -5,6 +5,7 @@ export default [
    route("login", "routes/login/login.tsx"), // Página de login
    route("register", "routes/register/register.tsx"), // Página de registro
    route("posts", "routes/post/posts.tsx"), // Listagem de todos os posts
+   route("createpost", "routes/post/createpost.tsx"), // Página para criar um novo post
    route("post/:id", "routes/post/post.tsx") // Detalhe de um post específico
 
 ] satisfies RouteConfig;
