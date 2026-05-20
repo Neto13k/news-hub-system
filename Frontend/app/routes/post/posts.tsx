0b256096ebdf0d5128827b48b74ba0e2f17ed96c
José Hermes <jose.hermes.dev@gmail.com>
2026-05-20 17:43:18 -0300

    feat: Preencher rota de posts com componente de listagem

---
+++ Frontend/app/routes/post/posts.tsx
@@ -0,0 +1,6 @@
+import RenderPosts from "../../components/posts";
+
+// Página de rota de posts que renderiza o componente de listagem de posts
+export default function PostsPage() {
+    return <RenderPosts />
+}
