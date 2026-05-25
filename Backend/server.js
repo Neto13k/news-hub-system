52f2b7990b5725395cd71ad5d15062ec3aff0505
José Hermes <jose.hermes.dev@gmail.com>
2026-05-25 20:47:34 -0300

    feat: Adicionar rota de notícias ao servidor

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,13 @@ const app = express();
 const cors = require('cors')
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
+const newsRoutes = require('./routes/newsRoutes');
 
 app.use(express.json())
 app.use(cors())
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
+app.use('/news', newsRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
